c1b1c04115966a8f116134ea103f7d10ca6804dd
alauddinDipu <alauddin.cucse@gmail.com>
2024-10-20 09:58:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const course = require("./data/course.json");
+const books =require("./data/books.json");
+
+app.use(cors());
+
+// app.use(cors({
+//   origin: ["http://localhost:5173", "https://course-react-routing-firebase.web.app"]
+// }));
+
+//server message
+app.get("/", (req, res) => {
+  res.send("Course Server Running");
+});
+
+app.get("/bookcategories", (req, res) => {
+  res.send(books);
+});
+
+
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  id = req.params.id;
+  // console.log(id);
+  const selectedProducts = course.find(n => n._id === id);
+  res.send(selectedProducts);
+});
+
+app.get("/bookcategories/:category", (req, res) => {
+  category = req.params.category;
+  // console.log(id);
+  const selectedCategory = books.find(n => n.category === category);
+  res.send(selectedCategory);
+});
+
+app.listen(port, () => {
+  console.log(`Course Server is running on ${port}`);
+});
